630fc4e5a31b42a3c66ee64cca80e2a206b7739c
Nurul-GC <nuruldecarvalho@gmail.com>
2022-07-21 06:22:16 +0100

    update pgc-gui/*

--- pgc-gui/__init__.py
+++ pgc-gui/__init__.py
@@ -1,16 +1,16 @@
 from random import randint
-from sys import argv, exit
+from sys import argv
 from time import sleep
 
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
+
+from mainwindow import PGC
 
 
 class PGCINIT:
     def __init__(self):
-        self.gc = QApplication(argv)
-
         # application font
         QFontDatabase.addApplicationFont("./font/lemon.ttf")
 
@@ -33,76 +33,10 @@ class PGCINIT:
             load += randint(5, 10)
         self.janela.close()
         app = PGC()
-
-
-class PGC:
-    def __init__(self):
-        theme = open('./theme/pgc.qss').read().strip()
-
-        self.ferramentas = QWidget()
-        self.ferramentas.setFixedSize(500, 300)
-        self.ferramentas.setWindowTitle("PlayGC")
-        '''self.ferramentas.setStyleSheet("""
-background-image: url("./favicon/bg.jpg");
-background-repeat: no-repeat;
-background-size: auto;
-background-attachment: fixed;
-background-position: center;
-""")'''
-
-        # ******* background-image *******
-        bg_image = QImage(f"./favicon/bg.jpg").scaled(QSize(500, 300))
-        palette = QPalette()
-        palette.setBrush(palette.ColorGroup.All, palette.ColorRole.Window, QBrush(bg_image))
-        self.ferramentas.setPalette(palette)
-
-        self.layout = QFormLayout()
-        self.layout.setSpacing(20)
-
-        menu = QMenuBar()
-        detalhes = menu.addMenu("Help")
-        instr = detalhes.addAction("Instruction")
-        sair = detalhes.addAction("Exit")
-        sobre = menu.addAction("About")
-        self.layout.setMenuBar(menu)
-
-        introlabel = QLabel("<h1>Convert your Videos to Audio</h1><hr>")
-        self.layout.addRow(introlabel)
-
-        videolayout = QHBoxLayout()
-        self.nomevideo = QLineEdit()
-        self.nomevideo.setReadOnly(True)
-        videolayout.addWidget(self.nomevideo)
-        procvideo = QPushButton("Search Video")
-        procvideo.clicked.connect(self.procurar_video)
-        videolayout.addWidget(procvideo)
-        self.layout.addChildLayout(videolayout)
-
-        convertbtn = QPushButton("Convert")
-        convertbtn.clicked.connect(self.convert)
-        self.layout.addRow(convertbtn)
-
-        # copyright-label
-        link = lambda: webbrowser.open_new('https://artesgc.home.blog')
-        copyrightlabel = QLabel('<a style="text-decoration: none; background-color: none;" href="#">&trade;ArtesGC, Inc</a>')
-        copyrightlabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        copyrightlabel.linkActivated.connect(link)
-        copyrightlabel.setToolTip("Open ArtesGC's oficial website!")
-
-        barramenu = QToolBar("Copyright")
-        barramenu.setLayoutDirection(Qt.LayoutDirection.RightToLeft)
-        barramenu.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonFollowStyle)
-        barramenu.addWidget(copyrightlabel)
-        self.layout.addRow(barramenu)
-        self.ferramentas.show()
-
-    def procurar_video(self):
-        pass
-
-    def convert(self):
-        pass
+        app.ferramentas.show()
 
 
 if __name__ == '__main__':
+    gcApp = QApplication(argv)
     pgcApp = PGCINIT()
-    pgcApp.gc.exec()
+    gcApp.exec()
